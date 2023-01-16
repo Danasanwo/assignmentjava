4db35fae7dab64027ea3fed7c94f1d9edd9e734b
Damilola Anasanwo <damilolaanasanwo@Damilolas-MacBook-Air.local>
2023-01-16 05:03:45 +0000

    rectangle javadoc

--- Triangle.java
+++ Triangle.java
@@ -1,3 +1,10 @@
+/**
+* <h2> This is the documentation of the Triangle Class" </h2>
+* <p> This class contains the basic properties of the triangle shape
+* </p>
+* @author Damilola Anasanwo
+* 
+*/
 public class Triangle extends Shape {
 
 }
