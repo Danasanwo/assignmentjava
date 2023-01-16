dd88ea15f77988ec53cd9cbe0079b43ec6864e83
Damilola Anasanwo <damilolaanasanwo@Damilolas-MacBook-Air.local>
2023-01-16 04:32:49 +0000

    more javadocs

--- Main.java
+++ Main.java
@@ -1,3 +1,10 @@
+/**
+* <h2> This is the documentation of the Main Class" </h2>
+* <p> This class contains the main class used to initiate the application
+* </p>
+* @author Damilola Anasanwo
+* 
+*/
 public class Main {
     public static void main(String[] args) {
 
