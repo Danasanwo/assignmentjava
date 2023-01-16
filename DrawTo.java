acb03a9cd6db975e80dc8636bdd1108128a1b221
Damilola Anasanwo <damilolaanasanwo@Damilolas-MacBook-Air.local>
2023-01-16 05:02:40 +0000

    draw javadoc

--- DrawTo.java
+++ DrawTo.java
@@ -1,3 +1,10 @@
+/**
+* <h2> This is the documentation of the Drawto Class" </h2>
+* <p> This class contains the basic properties of the Drawto Method of the visual language
+* </p>
+* @author Damilola Anasanwo
+* 
+*/
 public class DrawTo extends Shape {
     public Integer newPositionX;
     public Integer newPositionY;
