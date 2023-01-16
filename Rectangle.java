5a48d02b73332ff3840e70225d74fc68f0590db7
Damilola Anasanwo <damilolaanasanwo@Damilolas-MacBook-Air.local>
2023-01-16 04:59:33 +0000

    rectangle javadoc

--- Rectangle.java
+++ Rectangle.java
@@ -1,3 +1,10 @@
+/**
+* <h2> This is the documentation of the Rectangle Class" </h2>
+* <p> This class contains the basic properties of the rectangle shape
+* </p>
+* @author Damilola Anasanwo
+* 
+*/
 public class Rectangle extends Shape {
     public Integer width;
     public Integer height;
